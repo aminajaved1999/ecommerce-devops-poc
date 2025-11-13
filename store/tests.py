3314809471c4a8ac6ecfa7454dd3abe98f1edc16
aminajaved1999 <aminajaved1999@gmail.com>
2025-11-13 09:23:27 +0500

    Add product model, view, and template for PoC

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+
+# Create your tests here.
+class StoreViewTests(TestCase):
+    def test_product_list_page_status_code(self):
+        response = self.client.get(reverse('product_list'))
+        self.assertEqual(response.status_code, 200)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product
+
+# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'store/product_list.html', context)
