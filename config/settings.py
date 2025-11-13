063667f9c0803d69a6f7087e6a15542fc8ed6e97
aminajaved1999 <aminajaved1999@gmail.com>
2025-11-13 09:26:02 +0500

    DEBUG = True

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p%hqur)!*do8^o$_3rtuu5q1k8s+h^m)7s6h=qx44+b7cb6g5v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'amina-devops-poc.onrender.com',
