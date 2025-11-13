26c34fcc164de9cf03a0783d2c9525f449e43ff5
aminajaved1999 <aminajaved1999@gmail.com>
2025-11-13 12:53:22 +0500

    local static image paths.

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+# Add this to the end of the file if not already present
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
